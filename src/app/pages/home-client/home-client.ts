371ffed5a5ffcb0cbec284125c59571bf99ea41a
cristianolivera1 <221181@unamba.edu.pe>
2025-08-01 19:26:40 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/pages/home-client/home-client.ts
+++ src/app/pages/home-client/home-client.ts
@@ -3,10 +3,11 @@ import { FooterClient } from '../../shared/footer-client/footer-client';
 import { HeaderClient } from '../../shared/header-client/header-client';
 import { SummaryClient } from './components/summary-client/summary-client';
 import { HeroClient } from './components/hero-client/hero-client';
+import { Prevention } from './components/prevention/prevention';
 
 @Component({
   selector: 'app-home-client',
-  imports: [HomeClient,FooterClient,HeaderClient,SummaryClient,HeroClient],
+  imports: [HomeClient,FooterClient,HeaderClient,SummaryClient,HeroClient,Prevention],
   templateUrl: './home-client.html',
   styleUrl: './home-client.css'
 })
